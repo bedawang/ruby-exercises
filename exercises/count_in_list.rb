ac12407310cc34486f6e3d185011d221d4b3f628
Christof Becu <christof.becu@gmail.com>
2018-01-03 22:57:07 +0100

    Update count_in_list.rb

--- exercises/count_in_list.rb
+++ exercises/count_in_list.rb
@@ -19,9 +19,19 @@ def count_in_list(list, item_to_count)
   #  1. A running total of the number of times you've seen the item
   #  2. A way to loop/iterate through the list
   #  3. A way to add to the running total as you see the item
+  count_so_far = 0
+  list.each {|item|
+    if item == item_to_count
+      count_so_far += 1
+    end
+  }
+  return count_so_far
 end
 
 if __FILE__ == $PROGRAM_NAME
   # I'd advise putting some sanity checks here.
   # How else will you be sure your code does what you think it does?
+  p count_in_list([1,2,3], 1) == 1
+  p count_in_list([1,2,3], -1) == 0
+  p count_in_list([1,1,1], 1) == 3
 end
