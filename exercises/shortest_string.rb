d818895192b072fb4fa747d1ee7f104e311e603f
Christof Becu <christof.becu@gmail.com>
2018-01-03 20:29:32 +0100

    Update shortest_string.rb

--- exercises/shortest_string.rb
+++ exercises/shortest_string.rb
@@ -4,10 +4,18 @@
 # Prints:      Nothing
 
 def shortest_string(list)
-  # This is your job. :)
+  shortest_so_far = list.first
+  list.each {|item|
+    if item.size < shortest_so_far.size
+      shortest_so_far = item
+    end
+  }
+  return shortest_so_far
 end
 
 if __FILE__ == $PROGRAM_NAME
+  p shortest_string(["a", "zzzz", "c"]) == "a"
+  p shortest_string(["aaa", "1111", "3"]) == "3"
   # I'd advise putting some sanity checks here.
   # How else will you be sure your code does what you think it does?
 end
